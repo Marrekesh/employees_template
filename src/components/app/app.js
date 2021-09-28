9e2210f4a5b2c6f74c9a2921be37d04542178613
Відділ правового забезпечення 3 <ZG3@das.dsa>
2021-09-28 15:53:19 +0300

    salary

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import {Component} from 'react'
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,87 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
 
-    const data = [
-        {name: 'John C.', salary: 800, increase: false, id:1},
-        {name: 'Alex M.', salary: 3000, increase: true, id:2},
-        {name: 'Carl W.', salary: 5000, increase: false, id:3}
-    ];
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{name: 'John C.', salary: 800, increase: false, rise:false, id:1},
+				{name: 'Alex M.', salary: 3000, increase: false, rise:false, id:2},
+				{name: 'Carl W.', salary: 5000, increase: false, rise:false, id:3}
+			]
+		}
+		this.maxId = 4
+	}
 
-	return (
-		<div className="app">
-			<AppInfo />
+	onDelete = (id) => {
+		this.setState(({data}) => {
+			const index = data.findIndex(elem => elem.id === id);
 
-			<div className="search-panel">
-				<SearchPanel/>
-				<AppFilter/>
+			const before = data.slice(0, index);
+			const after = data.slice(index + 1);
+
+			const newArr = [...before, ...after]
+
+			return {
+				data: newArr
+			}
+		})
+	}
+
+	onAdd = (name, salary) => {
+		const newItem = {
+			name: name,
+			salary: salary,
+			increase: false,
+			rise:false,
+			id: this.maxId++
+		}
+
+		this.setState(({data}) => {
+			const newArr = [...data, newItem]
+
+			return {
+				data: newArr
+			}
+		})
+	}
+
+	onToggle = (id, prop) => {
+		this.setState(({data}) => ({
+			data: data.map(item => {
+				if (item.id === id) {
+					return {...item, [prop]: !item[prop]}
+				}
+				return item
+			})
+		}))
+	}
+
+	render() {
+
+		const {data} = this.state
+
+		return (
+			<div className="app">
+				<AppInfo />
+
+				<div className="search-panel">
+					<SearchPanel/>
+					<AppFilter/>
+				</div>
+				
+				<EmployeesList 
+				data={data}
+				onDelete={this.onDelete}
+				onToggle={this.onToggle}/>
+				<EmployeesAddForm
+				onAdd={this.onAdd}/>
 			</div>
-			
-			<EmployeesList data={data}/>
-			<EmployeesAddForm/>
-		</div>
-	);
+		);
+	}
+
 }
 
-export default App;
+
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,11 +2,15 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete, onToggle}) => {
     const elements = data.map(item => {
         const {id, ...items} = item;
         return(
-            <EmployeesListItem key={id} {...items}/>
+            <EmployeesListItem 
+            key={id} 
+            {...items}
+            onDelete={() => onDelete(id)}
+            onToggle={(e) => onToggle(id, e.currentTarget.getAttribute('data-toggle'))}/>
         )
     })
 
